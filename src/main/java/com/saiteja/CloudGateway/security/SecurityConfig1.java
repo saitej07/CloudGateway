8cb4a05eb1448c4c6416516ffa8e213c1fa4d9f5
saitej07 <saitejabollabathini@gmail.com>
2025-08-23 20:33:34 +0530

    Removed all security related things

---
+++ src/main/java/com/saiteja/CloudGateway/security/SecurityConfig1.java
@@ -0,0 +1,28 @@
+//package com.saiteja.CloudGateway.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//@Configuration
+//@EnableWebFluxSecurity
+//public class SecurityConfig1 {
+//
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+//        serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .authorizeExchange(exchange -> exchange
+//                        .pathMatchers("/actuator/**",
+//                                "/favicon.ico",
+//                                "/order/**",
+//                                "/payment/**",
+//                                "/product/**")
+//                        .permitAll()
+//                        .anyExchange()
+//                        .authenticated())
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
+//      return serverHttpSecurity.build();
+//    }
+//}
