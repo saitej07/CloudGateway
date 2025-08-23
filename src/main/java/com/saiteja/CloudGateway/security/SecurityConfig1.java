dbebf2b8ffa51e2062c6dd971cabb428135a777a
saitej07 <saitejabollabathini@gmail.com>
2025-08-23 21:13:59 +0530

    Added keycloak configuration.

--- src/main/java/com/saiteja/CloudGateway/security/SecurityConfig1.java
+++ src/main/java/com/saiteja/CloudGateway/security/SecurityConfig1.java
@@ -1,28 +1,24 @@
-//package com.saiteja.CloudGateway.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-//import org.springframework.security.config.web.server.ServerHttpSecurity;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
-//
-//@Configuration
-//@EnableWebFluxSecurity
-//public class SecurityConfig1 {
-//
-//    @Bean
-//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
-//        serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
-//                .authorizeExchange(exchange -> exchange
-//                        .pathMatchers("/actuator/**",
-//                                "/favicon.ico",
-//                                "/order/**",
-//                                "/payment/**",
-//                                "/product/**")
-//                        .permitAll()
-//                        .anyExchange()
-//                        .authenticated())
-//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
-//      return serverHttpSecurity.build();
-//    }
-//}
+package com.saiteja.CloudGateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig1 {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/actuator/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
+      return serverHttpSecurity.build();
+    }
+}
